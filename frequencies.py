b90daee04dfa88efb6947e762d03179f04e14062
Elizabeth Worthington <jamjawjar@gmail.com>
2022-10-10 11:36:17 +0100

    fix code to key items as strings

--- frequencies.py
+++ frequencies.py
@@ -5,8 +5,9 @@ def frequencies(items):
     frequencies = {}
     # Your code goes here
     for item in items:
-        if (item in frequencies):
-            frequencies[item] += 1
+        if (str(item) in frequencies.keys()):
+            frequencies[str(item)] += 1
         else:
-            frequencies[item] = 1
+            frequencies[str(item)] = 1
     return frequencies
+    
